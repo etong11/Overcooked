5523b5e38ff8a91cb30d507606d241a294d02c6b
Emma Tong <eat2004@gmail.com>
2022-11-13 22:34:20 -0500

    Initialized

---
+++ main.py
@@ -0,0 +1,15 @@
+#################################################
+# Helper functions from 
+# https://www.cs.cmu.edu/~112/notes/notes-variables-and-functions.html#RecommendedFunctions
+#################################################
+
+def almostEqual(d1, d2, epsilon=10**-7):
+    # note: use math.isclose() outside 15-112 with Python version 3.5 or later
+    return (abs(d2 - d1) < epsilon)
+
+def roundHalfUp(d):
+    # Round to nearest with ties going away from zero.
+    rounding = decimal.ROUND_HALF_UP
+    # See other rounding options here:
+    # https://docs.python.org/3/library/decimal.html#rounding-modes
+    return int(decimal.Decimal(d).to_integral_value(rounding=rounding))
